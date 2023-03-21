f2e85348e217f21f744f0132c7b99e63302ff80c
Martín Morondo <martinmorondo@gmail.com>
2023-03-21 19:15:15 -0300

    add Tours component to the App to show a list of available tours

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
+import Tours from './Tours';
 
 const url = 'https://course-api.com/react-tours-project';
 
@@ -49,10 +50,10 @@ function App() {
     )
   }
 
-
-
   return (
-    
+    <main>
+      <Tours tours = {tours} removeTour = {removeTour} />
+    </main>
   )
 }
 
