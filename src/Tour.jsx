3b569281d6fa6988c8856bb413ffa9d8ab833342
Martín Morondo <martinmorondo@gmail.com>
2023-03-21 22:58:27 -0300

    add image slider with react-slick

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const Tour = ({ id, image, info, name, price, removeTour }) => {
+
+const Tour = ({ id, images, image, info, name, price, removeTour }) => {
 
     const [readMore, setReadMore] = useState(false);
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
 
     return (
         <article className='single-tour'>
-            <img src = {image} alt = {name} />
+             <Slider {...settings}>
+        {images.map((image, id) => (
+          <div key={id}>
+            <img src={image} alt={name} />
+          </div>
+        ))}
+      </Slider>
             <footer>
 
                 <div className='tour-info'>
